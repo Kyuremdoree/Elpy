c539293feb93d456ef0662462b8301c5e5347d72
L0ol4X <lolachalmin@gmail.com>
2024-03-23 19:43:51 -0400

    :bento: added the ConvertAudio xml v1

---
+++ Elpy/app/src/main/java/uqac/dim/elpy/ConvertAudio.java
@@ -0,0 +1,27 @@
+package uqac.dim.elpy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+public class ConvertAudio extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
