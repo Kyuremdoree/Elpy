7527556e8bec9156d6ff30647ca971ebdaabb427
alexis.laurent <alexis.laurent@etu.uca.fr>
2024-04-14 13:07:04 -0400

    Menu - Image | Try SaveInstance On Ping Map | String English Traduction

--- Elpy/app/src/main/java/uqac/dim/elpy/utilitaire/MarkerInfo.java
+++ Elpy/app/src/main/java/uqac/dim/elpy/utilitaire/MarkerInfo.java
@@ -1,10 +1,37 @@
 package uqac.dim.elpy.utilitaire;
 
-public class MarkerInfo {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.maps.model.Marker;
+
+public class MarkerInfo implements Parcelable {
 
     private String nom;
 
     private String description;
+
+    private Marker marker;
+
+
+    protected MarkerInfo(Parcel in) {
+        nom = in.readString();
+        description = in.readString();
+    }
+
+    public static final Creator<MarkerInfo> CREATOR = new Creator<MarkerInfo>() {
+        @Override
+        public MarkerInfo createFromParcel(Parcel in) {
+            return new MarkerInfo(in);
+        }
+
+        @Override
+        public MarkerInfo[] newArray(int size) {
+            return new MarkerInfo[size];
+        }
+    };
 
     public String getNom() {
         return nom;
@@ -30,8 +57,24 @@ public class MarkerInfo {
         this.description = description;
     }
 
-    public MarkerInfo(String nom, String description){
+    public MarkerInfo(String nom, String description, Marker marker){
         setNom(nom);
         setDescription(description);
+        this.marker = marker;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(nom);
+        dest.writeString(description);
+    }
+
+    public Marker getMarker() {
+        return marker;
     }
 }
